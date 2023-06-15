27808961a7f92469a96847860aa07d9a207e23c9
Raheleh.kheiri <r.kheiri9693@gmail.com>
2023-06-15 22:38:38 +0430

    practice 4 of algorithm

---
+++ practice4.js
@@ -0,0 +1,38 @@
+// // A
+// let N = 30;
+// let sum= 0;
+// for (let i = 3; i<= N; i+= 3){
+//     sum+= 1/i;
+// }
+// console.log(sum)
+
+// // B
+// let N = 30;
+// let X = 2
+// let sum= 0;
+// for (let i = 0; i<= N; i++){
+//     sum+= X**i;
+// }
+// console.log(sum)
+
+// // C
+// let N = 30;
+// let X = 2
+// let fact = 1;
+// let sin = 0;
+// for (let i = 3; i<= N; i++){
+//     let m = (2*i - 1)
+//     for (let p = m; p>= 1; p--){
+//         fact*= p;
+//     }
+//     sin = (X - (X**3)/6) + X**m/fact
+// }
+// console.log(sin)
+
+// // D
+// let N = 30;
+// let sum = 0;
+// for (let i = 2; i <= N; i ++){
+//     sum+= i*(i-1)
+// }
+// console.log(sum)
